31eda967427b33a504c82317b35439d1f238795e
Cem GEÇGEL <aa-albara@hotmail.com>
2021-03-13 12:56:20 +0300

    Süreç Sınıfı Taşındı
    Süreç sınıfı iskeletten kütüphaneye taşındı.

---
+++ kod/başaşağıderebeyi/kütüphane/matematik/Ortalama.java
@@ -0,0 +1,36 @@
+/**
+ * başaşağıderebeyi.kütüphane.matematik.Ortalama.java
+ * 0.11 / 11 Mar 2021 / 21:38:41
+ * Cem GEÇGEL (BaşAşağıDerebeyi)
+ */
+package başaşağıderebeyi.kütüphane.matematik;
+
+/** Bir değerin farklı örneklerinin ortalaması. */
+public class Ortalama {
+	private int sayacı;
+	private float toplamı;
+	private float ortalaması;
+	
+	/** Sayaçları sıfırlar. */
+	public void sıfırla() {
+		sayacı = 0;
+		toplamı = 0.0F;
+		ortalaması = 0.0F;
+	}
+	
+	/** Yeni bir değer ekler. */
+	public void ekle(final float değeri) {
+		toplamı += değeri;
+		ortalaması = toplamı / ++sayacı;
+	}
+	
+	/** Eklenen değerlerin toplamını döndürür. */
+	public float toplamınıEdin() {
+		return toplamı;
+	}
+	
+	/** Eklenen değerlerin ortalamasını döndürür. */
+	public float ortalamasınıEdin() {
+		return ortalaması;
+	}
+}
---
+++ kod/başaşağıderebeyi/kütüphane/matematik/Süreç.java
@@ -0,0 +1,46 @@
+/**
+ * başaşağıderebeyi.kütüphane.matematik.Süreç.java
+ * 0.1 / 19 Oca 2021 / 10:44:55
+ * Cem GEÇGEL (BaşAşağıDerebeyi)
+ * 
+ * BaşAşağıMotor'dan alındı.
+ * 0.7 / 29 Ağu 2020 / 19:23:14
+ */
+package başaşağıderebeyi.kütüphane.matematik;
+
+/** Bir işlemin bir saniyede her tık başına aldığı ortalama süreyi bulmaya
+ * yarayan araç. */
+public class Süreç {
+	private final Ortalama ortalaması;
+	private float başlangıcı;
+	
+	/** Sıfır süreç tanımlar. */
+	public Süreç() {
+		ortalaması = new Ortalama();
+	}
+	
+	/** Süreci başlatır. */
+	public void başla(final float zaman) {
+		başlangıcı = zaman;
+	}
+	
+	/** Süreci durdurur. */
+	public void dur(final float zaman) {
+		ortalaması.ekle(zaman - başlangıcı);
+	}
+	
+	/** Sayaçları sıfırlar. */
+	public void sıfırla() {
+		ortalaması.sıfırla();
+	}
+	
+	/** Saniye biriminden ortalama süreyi döndürür. */
+	public float ortalamasınıEdin() {
+		return ortalaması.ortalamasınıEdin();
+	}
+	
+	/** Saniye biriminden sürelerinin toplamını döndürür. */
+	public float toplamınıEdin() {
+		return ortalaması.toplamınıEdin();
+	}
+}
