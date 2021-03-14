05cb207b3e25d8f70b97d736449a1d85a16d37d9
Cem GEÇGEL <aa-albara@hotmail.com>
2021-03-14 21:00:21 +0300

    Kütüphane Sınıfı Eklendi
    Arayüzü yenilikleri 0.12 sürümünde ve bunu belirtmek için Kütüphane sınıfı oluşturuldu.
    Kütüphane sınıfı için şu anlık başka bir özellik düşünülmüyor.

---
+++ kod/başaşağıderebeyi/kütüphane/Kütüphane.java
@@ -0,0 +1,11 @@
+/**
+ * başaşağıderebeyi.kütüphane.Kütüphane.java
+ * 0.12 / 14 Mar 2021 / 20:58:41
+ * Cem GEÇGEL (BaşAşağıDerebeyi)
+ */
+package başaşağıderebeyi.kütüphane;
+
+public class Kütüphane {
+	/** Kütüphanenin sürümü. */
+	public static final String SÜRÜM = "0.12";
+}
