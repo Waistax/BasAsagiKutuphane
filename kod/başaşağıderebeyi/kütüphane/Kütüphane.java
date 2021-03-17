ec16e92cd31ac20899ed0e1a0b3b59ca55777390
Cem GEÇGEL <aa-albara@hotmail.com>
2021-03-17 10:33:06 +0300

    0.12.3
    Sürüm biçimi güncellendi.

--- kod/başaşağıderebeyi/kütüphane/Kütüphane.java
+++ kod/başaşağıderebeyi/kütüphane/Kütüphane.java
@@ -5,7 +5,19 @@
  */
 package başaşağıderebeyi.kütüphane;
 
+/** Kütüphane ile ilgili genel şeylerin bulunduğu sınıf. */
 public class Kütüphane {
-	/** Kütüphanenin sürümü. */
-	public static final String SÜRÜM = "0.12.2";
+	/** Ana sürümü. */
+	public static final int ANA_SÜRÜMÜ = 0;
+	/** Ara sürümü. */
+	public static final int ARA_SÜRÜMÜ = 12;
+	/** Yaması. */
+	public static final int YAMASI = 3;
+	/** Bütün sürümü. */
+	public static final String SÜRÜM =
+		ANA_SÜRÜMÜ + "." + ARA_SÜRÜMÜ + "." + YAMASI;
+	
+	/** Bu sınıfın bir nesnesini tanımlamayı engeller. */
+	private Kütüphane() {
+	}
 }
